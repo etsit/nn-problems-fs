e7c23e96197ee0a3cb7dc910ceb85d3594e536af
Daniel Wetterbro <daniel.wetterbro@pluspole.se>
2016-09-07 23:47:21 +0200

    Removed FsCheck example test ("non-empty list")

--- NnProblemsFs/Program.fs
+++ NnProblemsFs/Program.fs
@@ -1,11 +1,10 @@
 ﻿open System
-open P1Last.Tests
+open P1Last
 open P2LastButOne
 open P5Reverse
 open FsCheck
 
 [<EntryPoint>]
 let main argv = 
-    runP1Tests()
     Console.ReadLine() |> ignore
     0 // return an integer exit code
